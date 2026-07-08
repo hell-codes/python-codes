fc8b3f3337b9430962ce62a0a20bf1c23b74354d
Prabin Yadav <prabin.yadav935@gmail.com>
2026-07-09 00:19:37 +0530

    Add files via upload

---
+++ Functions/word.py
@@ -0,0 +1,5 @@
+#Takes word as an arguments and retuens its length
+def word_length(word):
+    return len(word)
+print(word_length("Hello"))
+print("The length of the word is:", word_length("Hello"))
