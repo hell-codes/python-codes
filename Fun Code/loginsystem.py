8489c073ba1d7be4fe642112f2687cc15a9a9ef1
Prabin Yadav <prabin.yadav935@gmail.com>
2026-07-20 00:22:32 +0530

    Add files via upload

---
+++ Fun Code/loginsystem.py
@@ -0,0 +1,7 @@
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+if username == "devil" and password == "1234":
+    print("Access granted 🔓")
+else:
+    print("Access denied 🔐")
