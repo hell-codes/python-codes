6a228ffbd6c2f1aea973b844a79817539b217c06
Prabin Yadav <prabin.yadav935@gmail.com>
2026-07-20 00:22:51 +0530

    Add files via upload

---
+++ Fun Code/numberguess.py
@@ -0,0 +1,8 @@
+import random
+number = random.randint(1, 10)
+guess = int(input("Guess a number between 1 and 10: "))
+
+if guess == number:
+    print("You guessed it right!")
+else:
+    print(f"Wrong! The correct number was {number}.")
